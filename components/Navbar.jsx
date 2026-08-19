d63beab911151716b2280ed4f992f2f9f1194ccb
Aqsa Wazeer <hareemrana522@gmail.com>
2026-08-19 17:52:01 +0500

    Remove inline styles from navbar logo so it perfectly matches the footer logo thickness and spacing

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -52,7 +52,7 @@ export default function Navbar() {
           <div className="logo-mark">
             <Image src="/logo.png.png" alt="Hashturn Logo" width={55} height={55} unoptimized style={{ objectFit: 'contain' }} />
           </div>
-          <span className="logo-text" style={{ color: "#000", fontFamily: '"Oswald", sans-serif', fontWeight: 900, letterSpacing: "0.5px" }}>HASHTURN</span>
+          <span className="logo-text" style={{ color: "#000" }}>HASHTURN</span>
         </Link>
 
         <ul className={`nav-links${open ? " open" : ""}`} id="navLinks">
